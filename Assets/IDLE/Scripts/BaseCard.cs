61880a02937138be6c455dd756ef30befa808071
Grindewald1900 <17788108425@163.com>
2023-07-22 00:22:07 -0400

    Create base class for card

---
+++ Assets/IDLE/Scripts/BaseCard.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCard : MonoBehaviour
+{
+    public string cardName;
+    public string cardDescription;
+    public int cardCost;
+    public Sprite cardImage;
+    public int cardPhysicalDamage;
+    public int cardSpiritualDamage;
+    public int cardMagicDamage;
+    public int cardHealth;
+    public int cardLevel;
+    public int cardPower;
+    public int cardStar;
+    public int cardArmor;
+    public int cardMagicResist;
+    public int cardSpiritualResist;
+    public int cardSpeed;
+    // Card position from 1 to 5
+    public int cardPriority;
+    // Mana, physical, spiritual 
+    public int cardAttackType;
+    // Random, front, back, all, highest atk, highest health,
+    public int cardTargetType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
